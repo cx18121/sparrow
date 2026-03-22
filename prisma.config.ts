c9b41b72a124f0c4a83727651fefa6f69564d511
cx18121 <charlie.l.xue@gmail.com>
2026-03-22 03:27:37 -0400

    update configs/dependencies

---
+++ prisma.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, env } from "@prisma/config";
+import { config } from "dotenv";
+
+config();
+
+export default defineConfig({
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
